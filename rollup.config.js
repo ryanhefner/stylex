493b6c1b9a407d7d67b738eef70b90ac10d3e0f8
Ryan Hefner <hi@ryanhefner.com>
2020-03-04 06:09:09 -0500

    Update Rollup config to support new version spec

--- rollup.config.js
+++ rollup.config.js
@@ -3,16 +3,25 @@ import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import pkg from './package.json';
 import { minify } from 'uglify-es';
 
 const config = {
   input: 'src/index.js',
-  name: 'stylex',
+  output: {
+    name: 'stylex',
+    file: './index.js',
+    format: 'umd',
+    globals: {},
+    banner: `/*! ${pkg.name} v${pkg.version} | (c) ${new Date().getFullYear()} Ryan Hefner | ${pkg.license} License | https://github.com/${pkg.repository} !*/`,
+    footer: '/* follow me on Twitter! @ryanhefner */',
+  },
+  external: [],
   plugins: [
     babel({
       exclude: 'node_modules/**',
+      externalHelpers: process.env.BABEL_ENV === 'umd',
     }),
     resolve(),
     commonjs({
@@ -23,14 +32,6 @@ const config = {
     }),
     json(),
   ],
-  external: [],
-  globals: {},
-  output: {
-    file: './index.js',
-    format: 'umd',
-  },
-  banner: `/*! ${pkg.name} v${pkg.version} | (c) ${new Date().getFullYear()} Ryan Hefner | ${pkg.license} License | https://github.com/${pkg.repository} !*/`,
-  footer: '/* follow me on Twitter! @ryanhefner */',
 };
 
 if (process.env.NODE_ENV === 'production') {
